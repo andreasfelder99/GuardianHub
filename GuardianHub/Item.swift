adfe421179e1851d8ff21cde04baedfc4b2042f9
Andreas Felder <andreas.felder@students.fhnw.ch>
2026-01-23 09:13:22 +0100

    chore: add .gitignore

---
+++ GuardianHub/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  GuardianHub
+//
+//  Created by Andreas Felder on 23.01.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ GuardianHubTests/GuardianHubTests.swift
@@ -0,0 +1,16 @@
+//
+//  GuardianHubTests.swift
+//  GuardianHubTests
+//
+//  Created by Andreas Felder on 23.01.2026.
+//
+
+import Testing
+
+struct GuardianHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
